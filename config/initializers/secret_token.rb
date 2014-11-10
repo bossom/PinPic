f5c6326aa28efe2da594b3f7d2cad937d94dd905
Sanna <janssonnordqvist@gmail.com>
2014-11-10 16:36:29 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PinPic::Application.config.secret_key_base = '524a787e3ba9693066aea545330d6fb76179787aa7da7015358e4cda0039e245a83898fa3de6a4681ae4328a4b8acbe4ced5d3b83e110ebdc99422f0e0ce173f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PinPic::Application.config.session_store :cookie_store, key: '_PinPic_session'
